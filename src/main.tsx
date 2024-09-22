0108ebdd350801b278a4b1e082d979f232819f64
grr1203 <gyrms9412@naver.com>
2024-09-22 23:49:09 +0900

    Home Page 구현

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router.tsx'
-import './reset.css';
+// import './reset.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+
 const Home = () => {
+  const [number1] = useState(2121);
+  const [number2] = useState(91);
+  const [notice] = useState('해당하는 창구로 상담받으러 오세요');
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col w-screen">
+      <div className="text-center text-7xl font-medium tracking-wider my-8 h-1/5">퇴원약국 복약상담실</div>
+      <div className="w-screen border-b border-gray-500 mb-6"></div>
+      <div className="h-2/5 relative pt-12 mb-10">
+        <img src="/line.png" className="w-full h-44" />
+        <div className="absolute inset-0 flex justify-between mx-14 w-full">
+          <div className="text-white text-8xl font-semibold mt-24 w-1/4 min-w-64">창구 1</div>
+          <div className="text-[150px] font-bold tracking-widest w-5/6 text-center">{number1}</div>
+        </div>
+      </div>
+      <div className="h-2/5 relative pt-12">
+        <img src="/line.png" className="w-full h-44" />
+        <div className="absolute inset-0 flex justify-between mx-14 w-full">
+          <div className="text-white text-8xl font-semibold mt-24 w-1/4 min-w-64">창구 2</div>
+          <div className="text-[150px] font-bold tracking-widest w-5/6 text-center">{number2}</div>
+        </div>
+      </div>
+      <div className="h-32 flex justify-center items-center mt-4 text-6xl font-bold text-center">{notice}</div>
     </div>
   );
 };
