8ac1d62e7051ba0e355d17c6f52a9192a90b7b1a
grr1203 <gyrms9412@naver.com>
2024-09-23 23:35:31 +0900

    번호 변경시 알림 소리 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useData } from '../hooks/useData';
 
 const Home = () => {
   const { data } = useData({});
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   // Server-Sent Events (SSE)로 서버의 이벤트 처리
   useEffect(() => {
     const eventSource = new EventSource(`/events`);
     eventSource.onmessage = function (event) {
       console.log('Received message:', event.data);
-      if (event.data === 'refresh') window.location.reload(); // 페이지 새로고침
+      if (event.data === 'refresh') {
+        localStorage.setItem('playAudio', 'true'); // 재생 플래그 설정
+        window.location.reload(); // 페이지 새로고침
+      }
     };
     eventSource.onerror = (error) => console.error('SSE Error:', error);
 
@@ -17,9 +21,26 @@ const Home = () => {
     return () => eventSource.close();
   }, []);
 
+  useEffect(() => {
+    const shouldPlayAudio = localStorage.getItem('playAudio');
+    if (shouldPlayAudio === 'true') {
+      const checkRefAndPlay = () => {
+        if (audioRef.current) {
+          audioRef.current.play();
+          localStorage.removeItem('playAudio'); // 재생 후 다시 플래그 제거
+        } else {
+          // audioRef가 아직 null인 경우, 500ms 후 다시 시도
+          setTimeout(checkRefAndPlay, 500);
+        }
+      };
+      checkRefAndPlay();
+    }
+  }, []);
+
   return (
     <div className="flex flex-col w-screen h-screen bg-[url(/image.png)] bg-cover bg-center">
       <div className="text-center text-7xl font-medium tracking-wider my-8">퇴원약국 복약상담실</div>
+      <audio src="/bell.mp3" ref={audioRef} />
       <div className="w-screen border-b border-gray-500 mb-8"></div>
       <div className="h-1/3 relative pt-16 mb-10">
         <img src="/line.png" className="w-full h-44" />
