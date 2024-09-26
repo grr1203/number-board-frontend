ac35c1007e7678f26827e8ac29ff60257f21c718
grr1203 <gyrms9412@naver.com>
2024-09-26 13:50:05 +0900

    Home UI 수정요구사항 반영

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -42,24 +42,24 @@ const Home = () => {
 
   return (
     <div className="flex flex-col w-screen h-screen bg-[url(/image.png)] bg-cover bg-center overflow-hidden">
-      <div className="text-center text-7xl font-medium tracking-wider my-8">퇴원약 복약상담실</div>
+      <div className="text-center text-7xl text-[#222] font-semibold tracking-wide my-8">퇴원약 복약상담실</div>
       <audio src="/bell.mp3" ref={audioRef} />
-      <div className="w-screen border-b border-gray-500 mb-8"></div>
-      <div className="h-1/3 relative pt-16 mb-10">
-        <img src="/line.png" className="w-full h-44" />
+      <div className="w-screen border-b border-gray-500 mb-2"></div>
+      <div className="h-1/3 relative pt-12 mb-6">
+        <img src="/line.png" className="w-full h-40" />
         <div className="absolute inset-0 flex justify-between mx-12 w-full">
-          <div className="text-white text-8xl font-semibold mt-28 w-1/6 min-w-72">창구 1</div>
-          <div className="text-[150px] font-bold tracking-widest w-5/6 text-center mt-2">{data?.number1}</div>
+          <div className="text-white text-[82px] font-semibold mt-16 w-1/6 min-w-72 -ml-4">창구 1</div>
+          <div className="text-[130px] font-bold tracking-wide w-5/6 text-center">{data?.number1}</div>
         </div>
       </div>
       <div className="h-1/3 relative pt-12">
-        <img src="/line.png" className="w-full h-44" />
+        <img src="/line.png" className="w-full h-40" />
         <div className="absolute inset-0 flex justify-between mx-12 w-full">
-          <div className="text-white text-8xl font-semibold mt-24 w-1/6 min-w-72">창구 2</div>
-          <div className="text-[150px] font-bold tracking-widest w-5/6 text-center">{data?.number2}</div>
+          <div className="text-white text-[82px] font-semibold mt-16 w-1/6 min-w-72 -ml-4">창구 2</div>
+          <div className="text-[130px] font-bold tracking-wide w-5/6 text-center">{data?.number2}</div>
         </div>
       </div>
-      <div className="h-1/6 flex justify-center items-center my-4 text-6xl font-bold text-center">{data?.notice}</div>
+      <div className="h-1/6 flex justify-center items-center my-5 text-6xl font-semibold text-[#444] text-center">{data?.notice}</div>
     </div>
   );
 };
